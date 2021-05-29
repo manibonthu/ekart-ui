396f94ac2556193ad84b8bdeb5a42aac91cfc1e0
Bonthu <Mani.Bonthu@Chubb.com>
2021-05-29 22:17:34 +0530

    add server file to serve angular app

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+app.use(requireHTTPS);
+app.use(express.static('./dist/ekart-ui'));
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/ekart-ui/' });
+});
+app.listen(process.env.PORT || 8080);
